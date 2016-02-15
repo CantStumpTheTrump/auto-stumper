3127cb921928dc6f1051575c22cce1c43e61718f
raj <rajesh@jewgle.ga>
2016-02-14 19:36:31 -0500

    memes
    :

---
+++ stump.py
@@ -0,0 +1,8 @@
+print('if u wanna know if u can be stump just use this ok')
+from random import choice
+Y = 'sori u can be stump v intoing sadness ok'
+N = 'ur like trump u cant be intoing stumpness ok v good job'
+name = ''
+name = input('gib name')+choice((N,Y))
+print(name)
+
